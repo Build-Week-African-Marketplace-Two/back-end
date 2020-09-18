fca58e67b316e0e857458f58451160518510049a
Ben Venker <bvemails@gmai.com>
2020-09-17 19:23:03 -0500

    auth router, users model, items route and users router, plus tests

--- api/server.js
+++ api/server.js
@@ -5,7 +5,10 @@ const cookieparser = require('cookie-parser');
 
 const server = express();
 
+const auth = require('../middleware/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
 const itemsRouter = require('../routes/items-router.js');
+const usersRouter = require('../routes/users-router.js');
 // const server = require('../index.js');
 
 server.use(helmet());
@@ -13,7 +16,9 @@ server.use(cors());
 server.use(express.json());
 server.use(cookieparser());
 
+server.use('/api/auth', authRouter);
 server.use('/api/items', itemsRouter);
+server.use('/api/users', auth, usersRouter);
 
 server.get('/', (req, res) => {
   res.json({ message: "it's alive!" });
---
+++ auth/auth-router.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { orWhereNotExists } = require('../data/config');
+
+const auth = require('../middleware/authenticate-middleware');
+const users = require('../models/users-model');
+const router = require('../routes/items-router');
+
+router.post('/register', async (req, res) => {
+  const username = req.body.username;
+
+  const duplicateuser = await users.findBy({ username });
+
+  if (duplicateuser.length > 0) {
+    res.status(400).json({ message: 'User already exists' });
+  } else {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    try {
+      await users.add(user);
+      res.status(200).json({ message: 'User created' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await users.findBy({ username });
+    if (user.length === 0) {
+      return res.status(401).json({
+        message: 'invalid credentials',
+      });
+    }
+    const passwordValid = bcrypt.compareSync(password, user[0].password);
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: 'Invalid credentials',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userID: user[0].id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie('token', token);
+    res.status(200).json({ token, message: `Welcome ${user[0].username}` });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/logout', async (req, res) => {
+  await res.clearCookie('token').end();
+});
+
+module.exports = router;
