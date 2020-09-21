7dd215618af7df8a64d6ae5fef964717a86a1574
Ben Venker <bvemails@gmai.com>
2020-09-21 18:58:13 -0500

    another fix?

--- models/users-model.js
+++ models/users-model.js
@@ -22,7 +22,7 @@ function findById(id) {
 }
 
 async function add(user) {
-  const id = await db('users').insert(user, 'id');
+  const id = await db('users').insert(user);
   return findById(id);
 }
 
