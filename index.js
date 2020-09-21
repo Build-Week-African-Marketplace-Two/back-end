74903402c8ab68959468110caf4546b496318217
Josh Dunson <jdunson98@yahoo.com>
2020-09-21 18:26:05 -0500

    fixing port again

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT =
   process.env.NODE_ENV === 'testing' ? 3400 : process.env.PORT || 3300;
 
  server.listen(process.env.PORT, () => {
-   console.log('DB_HOST: ', process.env.DB_HOST);
-   console.log(`\n=== Server listening on port ${PORT}===\n`);
+   //console.log('DB_HOST: ', process.env.DB_HOST);
+   console.log(`\n=== Server listening on port ${process.env.PORT}===\n`);
  });
 
 module.exports = server;
