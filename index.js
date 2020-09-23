688a330a9ecbb73c13b4aeea62474c3a5fc4720d
Ben Venker <bvemails@gmai.com>
2020-09-22 19:43:01 -0500

    let server listen

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = require('./api/server');
 const PORT =
   process.env.NODE_ENV === 'testing' ? 3400 : process.env.PORT || 3300;
 
-// server.listen(process.env.PORT, () => {
-//   //console.log('DB_HOST: ', process.env.DB_HOST);
-//   console.log(`\n=== Server listening on port ${process.env.PORT}===\n`);
-// });
+server.listen(process.env.PORT, () => {
+  //console.log('DB_HOST: ', process.env.DB_HOST);
+  console.log(`\n=== Server listening on port ${process.env.PORT}===\n`);
+});
 
 module.exports = server;
